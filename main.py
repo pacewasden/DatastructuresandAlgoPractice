87c3b4ac9cd71b3442c50db11b18c5871f189eb8
PACE WASDEN <pacewasden@gmail.com>
2022-02-01 04:26:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+arrayFun= [6,7,8,9,4,3,2,4,5,6]
+# print(type(array))
+# def reversed(array):
+#   start=0
+#   end= len(array) - 1
+#   while start < end:
+#     array[start], array[end] = array[end], array[start]
+#     start +=1
+#     end -=1
+#   return array
+
+#   reversed(arrayFun)
+  print(arrayFun)
